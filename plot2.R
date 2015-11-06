49d4116abe17e6ff8bf320bcc58c488cdaed4a3a
Eric Blossom <eb@comsec.com>
2015-11-06 13:30:26 -0800

    Add plot2 source and output image

---
+++ plot2.R
@@ -0,0 +1,62 @@
+### Download data from url, and plot of Global Active Power (kilowatts) vs date
+### with x-axis ticks at "Thu", "Fri", "Sat"
+### Output shall be in a 480x480 png file named plot2.png
+
+library(dplyr)
+
+skip_download = FALSE
+
+data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+data_file <- "data/household_power_consumption.zip"
+
+dates_to_keep <- c("2/1/2007", "2/2/2007")
+
+
+if (!dir.exists("data")) {
+    stopifnot(dir.create("data", mode="0755"))
+}
+
+if (!skip_download){
+    stopifnot(download.file(data_url, data_file, method="curl", mode="wb") == 0)
+}
+date_downloaded <- date()
+
+## The file is a .zip file, and we need to use unz to unpack and open the single file within it
+df <- read.csv(unz(data_file, "household_power_consumption.txt"),
+              header=TRUE, sep=";", na.strings=c("?"), stringsAsFactors=FALSE)
+
+
+## Before converting dates and times, subset to retain only rows that we're interested in
+## df <- df[df$Date %in% dates_to_keep,]    # Results in 2880 obs. of 9 variables
+df <- filter(df, Date %in% dates_to_keep)
+
+ok <- complete.cases(df)                 # Turns out all are complete
+df <- df[ok, ]
+
+
+convert_dt <- function(date, time) {
+    ## Convert date and time as found in data file into POSIXct
+    ##
+    ## Args:
+    ##   date: character vector in MM/DD/YYYY format (or vector of same)
+    ##   time: character vector in HH:MM:SS format (or vector of same)
+    ##
+    ## Returns:
+    ##   POSIXct object (or vector of same if date and time are vectors)
+    ##
+    ## Note:
+    ##   Given that actual timezone of data is unknown, assume UTC
+    ##
+    as.POSIXct(strptime(paste(date, time), "%m/%d/%Y %H:%M:%S", tz="GMT"))
+}
+
+## Convert Date and Time strings to POSIXct
+##df$When <- convert_dt(df$Date, df$Time)
+df <- mutate(df, When=convert_dt(Date, Time))
+
+
+png("plot2.png", width=480, height=480)
+
+plot(df$When, df$Global_active_power, type="l", ylab="Global Active Power (kilowatts)")
+
+dev.off()
